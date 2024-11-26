2df2e6e5fc23f8d04aa7d19d67d34254af397fb4
Ismail0991 <ismailmohammad00001@gmail.com>
2024-11-26 23:58:58 +0500

    Delete authRoutes.js

--- authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { registerUser, loginUser } = require("../controller/authController");
-
-const router = express.Router();
-
-
-router.post("/login", loginUser);       // Login user
-
-module.exports = router;
