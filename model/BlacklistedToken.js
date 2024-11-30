4da78a9fbc108f1426e396eaa9f05b7b260280b9
Ismail0991 <ismailmohammad00001@gmail.com>
2024-11-30 14:33:37 -0800

    Delete model/BlacklistedToken.js

--- model/BlacklistedToken.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const blacklistedTokenSchema = new mongoose.Schema({
-  token: { type: String, required: true },
-  expiresAt: { type: Date, required: true }, // Store the expiration date of the token
-});
-
-module.exports = mongoose.model("BlacklistedToken", blacklistedTokenSchema);
