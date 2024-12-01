857345596cf8604277ac53ba54dc21ecf4f9aeb2
Safi32 <138612632+Safi32@users.noreply.github.com>
2024-12-01 20:40:59 +0500

    Merging Branches

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const userRoutes = require("./router/userRoutes");
-const restaurantRoutes = require("./router/restaurantRoutes");
-const dealRoutes = require("./router/dealRoutes");
-const authMiddleware = require("./middleware/authMiddleware");
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+const userRoutes = require("./router/userRoutes")
+const restaurantRoutes = require("./router/restaurantRoutes")
+const dealRoutes = require("./router/dealRoutes")
+const authMiddleware = require("./middleware/authMiddleware")
 
-dotenv.config();
-connectDB();
+dotenv.config()
+connectDB()
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-// Routes
-app.use("/api/user", userRoutes);
-app.use("/api/restaurant", restaurantRoutes);
-app.use("/api/deals", dealRoutes);
+app.use("/api/user", userRoutes)
+app.use("/api/restaurant", restaurantRoutes)
+app.use("/api/deals", dealRoutes)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
 
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"))
 app.use("/api/protected", authMiddleware, (req, res) => {
-  res.status(200).json({ message: "You are authorized!" });
-});
+  res.status(200).json({ message: "You are authorized!" })
+})
