3b0a3d707e8a1ac1bb30673113d58d753e42e803
Ismail0991 <ismailmohammad00001@gmail.com>
2024-12-04 07:10:59 -0800

    Otp verification update

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { loginUser, logoutUser } = require("../controller/userController");
+const { sendOtp, verifyOtp, resetPassword, loginUser, logoutUser } = require("../controller/userController");
 
 const router = express.Router();
 
+// Login and Logout
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+
+// Forgot Password
+router.post("/forgot-password/send-otp", sendOtp); // Send OTP
+router.post("/forgot-password/verify-otp", verifyOtp); // Verify OTP
+router.post("/forgot-password/reset", resetPassword); // Reset password
 
 module.exports = router;
